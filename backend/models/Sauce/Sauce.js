c93a7f39997a43291c21a53da10a577be03419b0
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-05-15 01:48:57 +0200

    Sauce Model

--- backend/models/Sauce/Sauce.js
+++ backend/models/Sauce/Sauce.js
@@ -1,41 +1,79 @@
 const mongoose = require("mongoose");
 const {Schema, model} = mongoose;
+const capitalize = require("../../utils/capitalize");
+require("dotenv").config();
+
+const RegexConfig = require("../../config/regex.config");
+const SauceUtils = require("./Sauce.utils");
+
+
+const{
+    HOSTNAME,
+    PORT,
+    PROTOCOL
+} = process.env;
+
+const{
+    uuidv4Regex,
+    alphaRegex,
+    fileRegex
+} = RegexConfig;
 
 const SauceSchema = new Schema({
     userId: {
         type: String,
         required:true,
-        immutable:true
+        immutable:true,
+        match:uuidv4Regex
     },
     name: {
         type:String,
-        required:true
+        required:true,
+        minLength:4,
+        maxLength:30,
+        lowercase:true,
+        get:value => capitalize(value)
     },
     manufacturer:{
         type:String,
-        required:true
+        required:true,
+        minLength:4,
+        maxLength:30,
+        lowercase:true,
+        get:value => capitalize(value)
     },
     description:{
         type:String,
-        required:true
+        required:true,
+        minLength:10,
+        maxLength:280,
     },
     mainPepper:{
         type:String,
-        required:true
+        required:true,
+        minLength:4,
+        maxLength:20,
+        lowercase:true,
+        get:value => capitalize(value)
     },
     imageUrl:{
         type:String,
         required:true,
+        lowercase:true,
+        match:fileRegex,
+        set:image => `${PROTOCOL}://${HOSTNAME}:${PORT}/public/images/${image}` 
     },
     heat:{
         type: Number,
-        required:true
+        required:true,
+        min:1,
+        max:10
     },
     likes:{
         type: Number,
         default:0,
     },
-    dislike:{
+    dislikes:{
         type: Number,
         default:0,
     },
@@ -49,6 +87,8 @@ const SauceSchema = new Schema({
     }
 });
 
-const SauceModel = model('Sauce', SauceSchema);
+SauceSchema.statics.canAccess = SauceUtils.canAccess; 
+SauceSchema.statics.likeSauce = SauceUtils.likeSauce; 
 
+const SauceModel = model('Sauce', SauceSchema);
 module.exports = SauceModel;
