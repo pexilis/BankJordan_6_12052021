7cc5233c1db788c26742df639d55f748c492b14a
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-05-14 22:52:38 +0200

    UserModel

---
+++ backend/models/User/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
+const {v4: uuidv4} = require("uuid");
+const UserUtils = require("./User.utils");
+const {regexPassword} = require("../../config/regex.config");
+
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema({
+    userId: {
+        type: String,
+        required:true,
+        unique:true,
+        immutable:true,
+        set: id => uuidv4()
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true, 
+    },
+    password:{
+        type: String,
+        required:true,
+        minLength:12,
+        maxLength:256,
+        match:regexPassword,
+    }
+});
+
+userSchema.pre("save", UserUtils.hashBeforeSave)
+userSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator'});
+userSchema.statics.checkPassword = UserUtils.checkPassword;
+
+const userModel = model('User', userSchema);
+
+module.exports = userModel;
---
+++ backend/models/User/User.utils.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const aes = require("../../decoder/AES");
+
+const {
+    SALT_ROUNDS
+} = process.env; 
+
+const generatePassword = async(password) => {
+    let salt = await bcrypt.genSalt(Number.parseInt(SALT_ROUNDS));
+    let hash = await bcrypt.hash(password, salt);
+    return hash;
+}
+
+async function hashBeforeSave(element) {
+    const {password} = this;
+    const hashedPassword = await generatePassword(password);
+    this.password = hashedPassword;
+}
+
+async function checkPassword(email, password) {
+    const model = this;
+    const findUser = await model.findOne({email});
+    const userExist = (findUser !== null);
+
+    if (!userExist)
+        throw new Error();
+
+    const {password:hashedPassword} = findUser;
+    const isValidPassword = await bcrypt.compare(password, hashedPassword);
+    
+    if (!isValidPassword)
+        throw new Error();
+}
+
+module.exports = {
+    generatePassword,
+    hashBeforeSave,
+    checkPassword
+}
+
+
+
