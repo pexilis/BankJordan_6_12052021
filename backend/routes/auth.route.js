32146387b42fa8f85affe224dd075d6f32e0d7ac
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-05-20 17:06:20 +0200

    add auth routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const {simpleReq, complexReq} = require("../config/cors.config");
 const {isLogout} = require("../middleware/access.middleware");
 
+
+// Services 
+const login = require("../services/login.service");
+const register = require("../services/register.service");
+
 router.use(isLogout);
 router.options("*", cors(complexReq));
 router.get("*", cors(simpleReq));
@@ -12,11 +17,30 @@ router.post("*", cors(simpleReq));
 router.put("*", cors(simpleReq));
 router.delete("*", cors(simpleReq));
 
-router.post("/signup", (req, res) => {
-    
+router.post("/signup", async(req, res) => {
+    const {email, password} = req.body;
+
+    register(email, password).then(() => res.status(202).send({
+        message:"Si cette adresse est unique, votre compte sera réservé"
+    })).catch(err => {
+        const message = err.message.split(":")[2];
+        if (message.includes("Le mot de passe")){
+            res.status(400).send({
+                message
+            });
+        } else {
+            res.status(202).send({
+                message:"Si cette adresse est unique, votre compte sera réservé"
+            });
+        }
+    });
 });
 
 router.post("/login", (req, res) => {
+   const {email, password} = req.body; 
+
+   login(email, password).then(data => res.status(200).send(data))
+                         .catch(err => res.status(401).end())
    
 });
 
