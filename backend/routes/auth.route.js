70fa3afc6b259c62f59ac47c893b02a603548fb8
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-05-20 13:53:01 +0200

    add cors to route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const cors = require("cors");
+const {simpleReq, complexReq} = require("../config/cors.config");
+
+router.options("*", cors(complexReq));
+router.get("*", cors(simpleReq));
+router.post("*", cors(simpleReq));
+router.put("*", cors(simpleReq));
+router.delete("*", cors(simpleReq));
+
 router.post("/signup", (req, res) => {
     
 });
