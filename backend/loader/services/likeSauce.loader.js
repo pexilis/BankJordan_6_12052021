b21641ea52ff7df91f6e01ac6c04c5e35c3c9a5b
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-06-02 14:05:33 +0200

    add loaders for getSauces and likeSauce services

--- backend/loader/services/likeSauce.loader.js
+++ backend/loader/services/likeSauce.loader.js
@@ -1,10 +1,10 @@
 const LikeSauce = require("../../services/likeSauce.service");
 const configMessage = require("../../config/message.config");
-const sauceModel = require("../model/sauce.loader");
+const {sauceModel} = require("../model/sauce.loader");
 
 LikeSauce.init(
     sauceModel,
     configMessage
 );
 
-module.exports = LikeSauce;
+module.exports = LikeSauce.run;
