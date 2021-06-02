5d0f1dbcc865afd09747e9b4b9c90fa15c7f9843
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-06-02 16:57:16 +0200

    config: remove unused regex

--- backend/config/regex.config.js
+++ backend/config/regex.config.js
@@ -1,8 +1,5 @@
 const RegexConfig = (() => {
     let self = {};
-    self.regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-/:+-.]).{12,256}$/;
-    self.uuidv4Regex = /^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/;
-    self.alphaRegex = /^[a-zA-Z]{4,}$/;
     self.filenameRegex = /^[a-zA-Z0-9_-]{1,}.[a-zA-Z0-9]{1,}$/;
 
     return self;
